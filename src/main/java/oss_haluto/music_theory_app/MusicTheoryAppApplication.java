0e215bdb8753b7d8503af39f791ae89db4c5eefc
Haluto-oss <haluto.k@gmail.com>
2025-06-12 01:47:12 +0900

    Initial commit with Spring Boot
    First step

---
+++ src/main/java/oss_haluto/music_theory_app/MusicTheoryAppApplication.java
@@ -0,0 +1,13 @@
+package oss_haluto.music_theory_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicTheoryAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusicTheoryAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/oss_haluto/music_theory_app/MusicTheoryAppApplicationTests.java
@@ -0,0 +1,13 @@
+package oss_haluto.music_theory_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicTheoryAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
